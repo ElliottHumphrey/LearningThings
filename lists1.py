4539f0b7e8c732850146e4ee38f472fe188b8492
Harrison Hayes <HarrisonHayes97@yahoo.com>
2021-11-20 00:41:17 -0600

    Adding another comment.

--- lists1.py
+++ lists1.py
@@ -5,3 +5,6 @@ harrison_list_2 = [44, 3, 4, 6, 8, 9, 1, 2, 3, 4,
 # The above are my lists. You need to a new
 # list for yourself that consists of everything
 # in my two lists - but sorted in ascending order.
+
+# I made this changes from my own laptop! :D
+
